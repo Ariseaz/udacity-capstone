c9b44e1149b5acb3a4b8a5613df45e92c67a98fd
azeez <azeez@localhost.localdomain>
2020-01-17 18:57:43 +0100

    Refact: data; availability zone removed
    Resolve: #23

--- infrastructure/modules/eks/eks.tf
+++ infrastructure/modules/eks/eks.tf
@@ -9,9 +9,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.my-cluster.cluster_id
 }
 
-data "aws_availability_zones" "available" {
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
