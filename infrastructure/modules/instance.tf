ff938e72a965934b97e6af578d05f9c8e4c85a56
Azeez <adenijiazeez@gmail.com>
2020-01-18 19:52:05 +0100

    Refact: code error corrected

--- infrastructure/modules/instance.tf
+++ infrastructure/modules/instance.tf
@@ -1,21 +1,3 @@
-variable "ENV" {
-}
-
-variable "INSTANCE_TYPE" {
-  default = "t2.micro"
-}
-
-variable "PUBLIC_SUBNETS" {
-  type = list
-}
-
-variable "VPC_ID" {
-}
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
