116f6f098b8d9c3e65afc824af0af1b170442ee6
Azeez <adenijiazeez@gmail.com>
2020-01-18 10:00:11 +0100

    Refact: var.VPC_ID removed

--- infrastructure/modules/eks/securitygroups.tf
+++ infrastructure/modules/eks/securitygroups.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "demo-cluster" {
   name        = "terraform-eks-demo-cluster"
   description = "Cluster communication with worker nodes"
-  vpc_id      = var.VPC_ID
+  vpc_id      = module.main-vpc.vpc_id
 
   egress {
     from_port   = 0
