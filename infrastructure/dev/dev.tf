f29a33c184d5dfbf515495b957281dbe3985f10c
Azeez <adenijiazeez@gmail.com>
2020-01-18 20:31:21 +0100

    Refact: resolving issues

--- infrastructure/dev/dev.tf
+++ infrastructure/dev/dev.tf
@@ -1,4 +1,4 @@
-module "vpc" {
+module "main-vpc" {
   source     = "../modules/vpc"
   ENV        = "dev"
   AWS_REGION = var.AWS_REGION
--- infrastructure/modules/vpc/variables.tf
+++ infrastructure/modules/vpc/variables.tf
@@ -8,18 +8,3 @@ variable "cluster-name" {
   default = "terraform-eks-demo"
   type    = string
 }
-
-variable "INSTANCE_TYPE" {
-  default = "t2.micro"
-}
-
-variable "PUBLIC_SUBNETS" {
-  type = list
-}
-
-variable "VPC_ID" {
-}
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
-}
