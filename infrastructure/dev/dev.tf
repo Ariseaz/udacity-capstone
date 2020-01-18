61d1e6edb6dc379c180d4dd553a55a651cb92665
Azeez <adenijiazeez@gmail.com>
2020-01-18 19:55:28 +0100

    Refact: code refactored

--- infrastructure/dev/dev.tf
+++ infrastructure/dev/dev.tf
@@ -3,6 +3,5 @@ module "modules" {
   ENV        = "dev"
   AWS_REGION = var.AWS_REGION
   VPC_ID         = module.main-vpc.vpc_id
-  PRIVATE_SUBNETS = module.main-vpc.private_subnets
   PUBLIC_SUBNETS = module.main-vpc.public_subnets
 }
