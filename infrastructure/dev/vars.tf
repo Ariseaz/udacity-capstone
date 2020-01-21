fdc11a14409f86778759f31cd2919428e4445ec1
Azeez <adenijiazeez@gmail.com>
2020-01-21 04:34:24 +0100

    Refactor: code refactored

--- infrastructure/dev/vars.tf
+++ infrastructure/dev/vars.tf
@@ -6,4 +6,12 @@ variable "cluster-name" {
   default = "terraform-eks-dev"
   type    = string
 }
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
  
