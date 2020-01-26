28f06671caad146574603a509100690147c2e9ea
Azeez <adenijiazeez@gmail.com>
2020-01-26 13:29:16 +0100

    Refact: output file reworked

--- infrastructure/dev/output.tf
+++ infrastructure/dev/output.tf
@@ -1,3 +1,11 @@
 output "kubeconfig" {
   value = "${module.main-vpc.kubeconfig}"
 }
+
+output "kubeconfig" {
+  value = "${module.main-vpc.config-map-aws-auth}"
+}
+
+output "jenkins-ip" {
+  value = "modules/jenkins-ip"
+}
